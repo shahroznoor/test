7205b0f6c1977c06474ed6ecacd523faa94a2250
shahroznoor <52856620+shahroznoor@users.noreply.github.com>
2023-08-08 19:26:07 +0500

    inital commit

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/check", async (req, res) => {
-    try {
-        res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-        res.status(200).json({ check: false })
-    } catch { }
+// app.get("/check", async (req, res) => {
+//     try {
+//         res.setHeader('Content-Type', 'text/html');
+//         res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//         res.status(200).json({ check: false })
+//     } catch { }
+// })
+
+// app.listen(3000, () => console.log("server is running..."));
+
+
+// index.js
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
 })
 
-app.listen(3000, () => console.log("server is running..."));
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
